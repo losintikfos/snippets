26198ec1add6d3a185dab424e55ef6fd19507ad5
bdadson <bright@fastvision.com>
2013-03-20 17:09:26 +0000

    Simulate 802.1ad VLAN with Ryu

--- python/vlan_simulate.py
+++ python/vlan_simulate.py
@@ -2,7 +2,8 @@
 Requirement psuedocode:
 @author: bdadson
 
-PREAMBLE:   Customer specific 802.1ad VLAN in TOR(Top of Rack),
+PREAMBLE:   Install VLAN on switches for customers.
+            Customer specific 802.1ad VLAN in TOR(Top of Rack),
             using multi switch ports segregation via Q-in-Q. 
 
 
@@ -58,8 +59,16 @@ loop until value_pair.end() |key|
 
 '''
 from ryu.base.app_manager import RyuApp
+from ryu.ofproto import ether
 
 class SimulateStargateVlan(RyuApp):
+    
+    VLAN_TAG_802_1Q = 0x8100  # VLAN-tagged frame (IEEE 802.1Q) & Shortest Path Bridging IEEE 802.1aq
+    BRIDGE_TAG_802_1AD = 0x88A8  # Provider Bridging (IEEE 802.1ad) & Shortest Path Bridging IEEE 802.1aq
+    
+    _CONTEXTS = {'dpset': dpset.DPSet}
+    OFP_VERSIONS = [ofproto_v1_2.OFP_VERSION]
+    
     def __init__(self, *_args, **_kvargs):
        super(SimulateStargateVlan, self).__init__(*_args, **_kvargs)
 
@@ -67,62 +76,67 @@ class SimulateStargateVlan(RyuApp):
     Add  a new flow entry to the the switch flow table
     
     '''
-    def _add_flow(self, dp, match, actions):
-        inst = [dp.ofproto_parser.OFPInstructionActions(
-            dp.ofproto.OFPIT_WRITE_ACTIONS, actions)]
+    def _add_flow(self, datapath, match, actions):
+        inst = [datapath.ofproto_parser.OFPInstructionActions(
+            datapath.ofproto.OFPIT_WRITE_ACTIONS, actions)]
     
-        mod = dp.ofproto_parser.OFPFlowMod(
-            dp, cookie=0, cookie_mask=0, table_id=0,
-            command=dp.ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
+        mod = datapath.ofproto_parser.OFPFlowMod(
+            datapath, cookie=0, cookie_mask=0, table_id=0,
+            command=datapath.ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
             priority=0xff, buffer_id=0xffffffff,
-            out_port=dp.ofproto.OFPG_ANY, out_group=dp.ofproto.OFPG_ANY,
+            out_port=datapath.ofproto.OFPG_ANY, out_group=datapath.ofproto.OFPG_ANY,
             flags=0, match=match, instructions=inst)
         
-        dp.send_msg(mod)
+        datapath.send_msg(mod)
         
-    def tag_vlan(port, vlan_id, dp):
-        match = dp.ofproto_parser.OFPMatch()
-        match.set_in_port(in_port)
-        match.set_dl_type(eth_IP)
-        match.set_vlan_vid(m_vid)
-        actions = [dp.ofproto_parser.OFPActionPopVlan(),
-                   dp.ofproto_parser.OFPActionOutput(out_port, 0)]
-        self._add_flow(dp, match, actions)
+    def build_match(self, port):
+        match = datapth.ofproto_parser.OFPMatch()
+        match.set_in_port(port)
+        match.set_dl_type(ether.ETH_TYPE_IP)
+        return match
+    
+    def tag_vlan(self, port, vlan_id, datapath):
+        field = datapath.ofproto_parser.OFPMatchField.make(
+            datapath.ofproto.OXM_OF_VLAN_VID, vlan_id)
+        
+        actions = [datapath.ofproto_parser.OFPActionPushVlan(BRIDGE_TAG_802_1AD),
+            datapath.ofproto_parser.OFPActionPushVlan(VLAN_TAG_802_1Q),
+            datapath.ofproto_parser.OFPActionSetField(field)]
+        self._add_flow(datapath, self.build_match(port), actions)
     
     
     '''
-    Add all customer host(s) to VLAN using Q-in-Q.
-    Ethertype: 0x8100
+    Add customer host(s) to VLAN using Q-in-Q.
+    Ethertype: 0x8100, 0x88a8
     '''
-    def tag_customer_vlan(value, vlan_id, dp):
+    def tag_customer_vlan(self, alue, vlan_id, datapath):
         for port in value:
             print "Tagging port=>", port, "with VLAN_ID =>", vlan_id
-            tag_vlan(port, vlan_id, dp)
+            self.tag_vlan(port, vlan_id, datapath)
         print '\n',
     
     
     '''
     Ethertype: 0x88a8
     '''
-    def tag_trunk(port, trunk_id, dp):
-        pass
+    def tag_trunk(port, trunk_id, datapath):
+        actions = [datapath.ofproto_parser.OFPActionPushVlan(BRIDGE_TAG_802_1AD)]
+        self._add_flow(datapath, self.build_match(port), actions)
     
     
-    def tag_trunk_vlan(value, trunk_id, dp):
+    def tag_trunk_vlan(value, trunk_id, datapath):
         for port in value:
             print "Tagging port=>", port, "with TRUNK_ID", trunk_id
-            tag_trunk(port, trunk_id, dp)
+            self.tag_trunk(port, trunk_id, datapath)
     
     
-    def test():
-        #Static value of customer and aggregated switch ports
-        #Note this value is use purposely for test only
-        value_pair = ({
-        'cust1'    :    ['s0_eth1', 's0_eth2'],
-        'cust2'    :    ['s1_eth1', 's1_eth2', 's0_eth3'],
-        'cust3'    :    ['s2_eth0', 's2_eth1', 's1_eth3'],
-        'trunk'    :    ['s0_eth4', 's1_por4', 's2_eth4']
-        })#
+    def install_vpn_flow(datapath):
+        # Static value of customer and aggregated switch ports
+        # Note this value is use purposely for test only
+        value_pair = {'cust1'    :    ['s0_eth1', 's0_eth2'],
+                      'cust2'    :    ['s1_eth1', 's1_eth2', 's0_eth3'],
+                      'cust3'    :    ['s2_eth0', 's2_eth1', 's1_eth3'],
+                      'trunk'    :    ['s0_eth4', 's1_por4', 's2_eth4']}
         
         trunk_id, vlan_id = -1, -1
         for key, value in reversed(value_pair.items()):
@@ -130,18 +144,24 @@ class SimulateStargateVlan(RyuApp):
             
             if'cust' in key:
                 vlan_id += 1
-                self.tag_customer_vlan(value, vlan_id, dp)
+                self.tag_customer_vlan(value, vlan_id, datapath)
         
             elif 'trunk' in key:
                 trunk_id += 1
-                self.tag_trunk_vlan(value, trunk_id, dp)
+                self.tag_trunk_vlan(value, trunk_id, datapath)
+    
+    '''
+    Install DataPath event dispatcher to invoke this method
+    anytime there's event dispatched to the DataPath from controller
+    '''
+    @set_ev_cls(dpset.EventDP, dpset.DPSET_EV_DISPATCHER)
+    def handler_datapath(self, event):
+        if event.enter:
+            self.install_vpn_flow(event.dp)
     
 if(__name__ == "__main__"):
-    test()
     if False:
         h = 7
         while  h:
             print (h  if ((h - 1) > -1) else h + 1) 
             h -= 1
-         
-
