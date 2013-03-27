8f8932225c8cba0b2a2cd3898af6308c58aa4767
bdadson <bright@fastvision.com>
2013-03-27 17:23:10 +0000

    Topology simulation with Mininet

--- python/mininet/topo.py
+++ python/mininet/topo.py
@@ -33,38 +33,33 @@ Connect the switches together
         +(s2)->(s3)
         +(s1)->(s3)
         ).link()
-'''
-def build_network():
-    topo = SimulatingVlanTopo(s=3, n=4)
-    network = Mininet(topo, switch=OVSKernelSwitch);
-    network.run(CLI, network)
-    
+'''  
 from mininet.topo import Topo
 from mininet.net import Mininet
-# from mininet.log import setLogLevel
+
 from mininet.node import OVSKernelSwitch
 from mininet.cli import CLI
 class SimulatingVlanTopo(Topo):
      
     # default topology with minimum of 1 switch and node
-    
-    
     def __init__(self, s=1, n=1, **opts):
         super(SimulatingVlanTopo, self).__init__(**opts)
         self.configure(s, n)
-        self.assign_ipv4_addr()
      
-    
-    def assign_ipv4_addr(self):
-        pass
-                
-        
-
     def link_switches(self, p_switch, switch):
         if p_switch is not None:
             self.addLink(p_switch, switch)
         return p_switch
     
+    def assign_ipv4_addr(self, switch, host, index):
+        def default():
+            print("No switch found matching {0}", switch)
+        {
+            's1': lambda : host.setIP("192.168.0." + str(index + 1)),
+            's2': lambda : host.setIP("192.168.1." + str(index + 1)),
+            's3': lambda : host.setIP("192.168.2." + str(index + 1))
+         }.get(switch, default)()
+         
     
     def configure(self, s, n):
         '''
@@ -83,10 +78,24 @@ class SimulatingVlanTopo(Topo):
             p_switch = self.link_switches(p_switch, switch)
             if (p_switch is not None) and (s in p_switch):
                 self.addLink(p_switch, 's%s' % (s - (s - 1)))
-                
+                 
+def _assign_host_ip(fn):
+    if fn is None:
+        return
+    
+    topo = fn();
+    net = Mininet(topo)
+    hosts, switches = net.hosts, net.switches
+     
+    n = 0
+    for node in hosts:
+        print '{0} -> {1} -> {2}'.format(n, node.name, node.name[:2])
+        topo.assign_ipv4_addr(node.name[:2], node, n)
+        
+    for switch in switches:
+        topo.assign_ipv4_addr(switch.name, switch, 0)
+            
+fn = lambda : SimulatingVlanTopo(s=3, n=4)
 
-'''if __name__ == '__main__':
-    setLogLevel('info')
-    build_network();'''
- 
-topos = {'simulating_vlan_topo': lambda : SimulatingVlanTopo(s=3, n=4)}
+_assign_host_ip(fn)
+topos = {'simulating_vlan_topo': fn}
